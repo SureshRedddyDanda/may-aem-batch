e8a31f248b07409cdc8e5816548ad00da29d89ea
SureshReddyDanda <sureshreddydanda07@gmail.com>
2025-05-27 22:27:39 +0530

    may-aem-batch

--- core/src/main/java/com/adobe/aem/may/batch/core/models/DigitalParent.java
+++ core/src/main/java/com/adobe/aem/may/batch/core/models/DigitalParent.java
@@ -2,13 +2,21 @@ package com.adobe.aem.may.batch.core.models;
 
 import java.util.List;
 
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.resource.Resource;
+import org.apache.sling.api.resource.ResourceResolver;
 import org.apache.sling.models.annotations.DefaultInjectionStrategy;
 import org.apache.sling.models.annotations.Model;
 import org.apache.sling.models.annotations.injectorspecific.ChildResource;
+import org.apache.sling.models.annotations.injectorspecific.ScriptVariable;
 import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
 
-@Model(adaptables = Resource.class,defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
+import com.day.cq.wcm.api.Page;
+
+@Model(adaptables = {Resource.class,SlingHttpServletRequest.class}, defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
 public class DigitalParent {
 
     @ValueMapValue
@@ -19,6 +27,37 @@ public class DigitalParent {
 
     @ValueMapValue
     public int articlenum;
+
+    @ScriptVariable
+    Page currentPage;
+
+    @ValueMapValue
+    @Named(value="jcr:createdBy")
+    public String author;
+
+    public String getArticleAuthorName(){
+        return author;
+    }
+
+    @Inject
+    ResourceResolver resolver;
+
+    public String getArticleTitle(){
+        return currentPage.getTitle();
+    }
+
+    public String getArticlePagePath(){
+        return currentPage.getPath();
+    }
+
+    public Page getArticlePageParent(){
+        return currentPage.getParent();
+    }
+
+    public String getWebContentNode(){
+        return resolver.getUserID();
+    }
+
 
     @ChildResource
     public List<DigitalChild> bookdetailswithmap;
